c54baabd94635deb343c96650f66b29a4648526e
keithnull <keith1126@126.com>
2019-08-23 20:39:57 +0800

    add parse for numbers

--- leptjson.c
+++ leptjson.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>  /* NULL */
 
 #define EXPECT(c, ch)       do { assert(*c->json == (ch)); c->json++; } while(0)
+#define ISDIGIT(ch)         ((ch) >= '0' && (ch) <= '9')
+#define ISDIGIT1TO9(ch)     ((ch) >= '1' && (ch) <= '9')
 
 typedef struct {
     const char *json;
@@ -31,6 +33,50 @@ static int lept_parse_literal(lept_context *c, lept_value *v, const char *litera
     return LEPT_PARSE_OK;
 }
 
+static int lept_parse_number(lept_context *c, lept_value *v)
+{
+    const char *end;
+    end = c->json;
+    if(*end == '-') { /* Skip minus sign but not posivitve sign*/
+        ++end;
+    }
+    if(*end == '0') { /* Zero must has only one digit */
+        ++end;
+        /* comment these because we should treat this error as LEPT_PARSE_ROOT_NOT_SINGULAR */
+        #if 0
+        if(*end == 'x' || *end == 'X'){ /* Binary floating-point expression is not allowed */
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+        #endif
+    } else { /* Nonzero should start with 1-9 */
+        if(!(ISDIGIT1TO9(*end))) {
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+        for(++end; ISDIGIT(*end); ++end); /* followed by 0 or more digits */
+    }
+    if(*end == '.'){    /* handle decimal */
+        ++end;
+        if(!ISDIGIT(*end)){ /* the decimal part must have at least one digit */
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+        for(++end; ISDIGIT(*end); ++end);   /* skip these decimal digits */
+    }
+    if(*end == 'e' || *end == 'E'){ /* handle exponent */
+        ++end;
+        if(*end == '+' || *end == '-'){ /* for exponent, both + and - are legal */
+            ++end;
+        }
+        if(!ISDIGIT(*end)){ /* must have one or more digits */
+            return LEPT_PARSE_INVALID_VALUE;
+        }
+        for(++end; ISDIGIT(*end); ++end); /* skip these digits */
+    }
+    v->n = strtod(c->json, NULL);
+    c->json = end;
+    v->type = LEPT_NUMBER;
+    return LEPT_PARSE_OK;
+}
+
 static int lept_parse_value(lept_context *c, lept_value *v)
 {
     switch (*c->json) {
@@ -43,7 +89,7 @@ static int lept_parse_value(lept_context *c, lept_value *v)
         case '\0':
             return LEPT_PARSE_EXPECT_VALUE;
         default:
-            return LEPT_PARSE_INVALID_VALUE;
+            return lept_parse_number(c, v);
     }
 }
 
@@ -60,6 +106,7 @@ int lept_parse(lept_value *v, const char *json)
         lept_parse_whitespace(&c);
         if (*(c.json) != '\0') {
             result = LEPT_PARSE_ROOT_NOT_SINGULAR;
+            v->type = LEPT_NULL;
         }
     }
     return result;
@@ -73,6 +120,6 @@ lept_type lept_get_type(const lept_value *v)
 
 double lept_get_number(const lept_value *v)
 {
-    assert(v != NULL);
+    assert(v != NULL && v->type == LEPT_NUMBER);
     return v->n;
 }
